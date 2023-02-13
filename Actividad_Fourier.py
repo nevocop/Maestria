e464b5dd088d3d40ecb9beec26bfdd6aa7aff70d
nevocop <nelsone.barrerat@autonoma.edu.co>
2023-02-12 22:46:42 -0500

    Coef. b0, pres. de coef. en arreglo, serie fourier

--- Actividad_Fourier.py
+++ Actividad_Fourier.py
@@ -17,7 +17,7 @@ Y = sym.Piecewise((-1,t <-T/4),                         # Se utiliza Piecewise p
                    (-1,True),)
 
 # Número de coeficientes que pide el ejercicio
-n = 8
+term = 8
 
 # Procedimiento para calcular la Serie de Fourier
 # Definición del intervalo en el cual se va a analizar la función
@@ -30,12 +30,52 @@ Integral = Y                                            # Cálculo de la integra
 Integrado = sym.integrate(Integral,(t,t_ini,t_fin))     # Evaluación de la integral entre los límites de la función
 an_0 = (2/T)*Integrado                                  # La evaluación de la integral se multiplica por (2/T)
 an_0 = sym.simplify(an_0)                               # Se utiliza la función simplify para guardar el resultado simplificado
-print('\nEl coeficiente a0 es: ', an_0)                # Se imprime el valor del coeficiente (a0)
+print('\nEl coeficiente a0 es: ', an_0)                 # Se imprime el valor del coeficiente (a0)
 
 # Cálculo de coeficientes (an)
 Integral = Y * sym.cos(n*W*t)                           # Cálculo de la integral                                           
 Integrado = sym.integrate(Integral,(t,t_ini,t_fin))     # Evaluación de la integral entre los límites de la función
 an = (2/T) * Integrado                                  # La evaluación de la integral se multiplica por (2/T)
 an = sym.simplify(an)                                   # Se utiliza la función simplify para guardar el resultado simplificado
-print('\nExpresión an: \n')                               # Impresión de la expresión (an)
-sym.pprint(an)
+print('\nExpresión an: \n')                             # Impresión de la expresión (an)
+sym.pprint(an)                                          # Muestra el resultado de una forma mas ordenada
+
+# Cálculo de coeficientes (bn)
+Integral = Y * sym.sin(n*W*t)                           # Cálculo de la integral
+Integrado = sym.integrate(Integral,(t,t_ini,t_fin))     # Evaluación de la integral entre los límites de la función
+bn = (2/T * Integrado)                                  # La evaluación de la integral se multiplica por (2/T)
+bn = sym.simplify(bn)                                   # Se utiliza la función simplify para guardar el resultado simplificado
+print('\nExpresión bn: \n')                             # Impresión de la expresión (bn)
+sym.pprint(bn)                                          # Muestra el resultado de una forma mas ordenada
+
+# Se evaluan los coeficientes y se colocan en arreglos
+a0 = an.subs(n,0)                                       # Inicialización de arreglo en el que se van a mostrar los coeficientes de an
+b0 = bn.subs(n,0)                                       # Inicialización de arreglo en el que se van a mostrar los coeficientes de bn
+an_ini = [a0] ; bn_ini = [b0]                           # Se almacenan los arreglos en las variables finales
+# Inicialización del arreglo donde se muestran la cantidad de coeficientes solicitados
+n_i = [0]                                               
+i = 1
+# Ciclo para recorrer y almacenar los coeficientes en los arreglos creados
+while (i<=term):                                        
+    avalor = an.subs(n,i)
+    bvalor = bn.subs(n,i)
+    an_ini.append(avalor)
+    bn_ini.append(bvalor)
+    n_i.append(i)
+    i = i+1
+
+coef_fourier = {'n' : n_i, 'an' :an_ini, 'bn' :bn_ini}  # Se imprimen los coeficientes de Fourier
+print('\n Se presentan los coeficientes: \n')
+for coef in coef_fourier:
+    print(coef, ':', coef_fourier[coef])
+
+# Elaboración de la serie de Fourier
+ser_Fou = a0 + 0*t                                      # Se crea la variable donde se almacenará la serie de Fourier
+i = 1
+# Ciclo para recorrer el arreglo creado y posteriormente mostrar la serie de Fourier
+while (i<=term):
+    ser_Fou = ser_Fou + an_ini[i]*sym.cos(i*W*t)
+    ser_Fou = ser_Fou + bn_ini[i]*sym.sin(i*W*t)
+    i = i+1
+print('\nLa serie de Fourier para la función dada es:\n') # Se imprime la serie de Fourier
+sym.pprint(ser_Fou)                                     # Muestra el resultado de una forma mas ordenada
