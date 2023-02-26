54039508fdd1902d5f21955932be997ffbaa4af3
nevocop <nelsone.barrerat@autonoma.edu.co>
2023-02-26 02:10:51 -0500

    Se termina el método runge kutta, con comentarios

--- Proyecto_final.py
+++ Proyecto_final.py
@@ -4,20 +4,25 @@
 # Método Runge Kutta
 # Definición de la función runge_kut
 
-def runge_kut(f,x_i,y_i,intervalo,xf):
-    y=[y_i]
-    x=[x_i]
-    h=intervalo
-    n=int((xf-x_i)/h)
+def runge_kut(f,x_i,y_i,intervalo,xf):                  # Definición de la función con sus respectivos parámetros
+    x=[x_i]                                             # Se crea la lista donde se almacenarán los valores de x
+    y=[y_i]                                             # Se crea la lista donde se almacenarán los valores de y    
+    h=intervalo                                         # Se crea la variable donde se almacena el intervalo a usar
+    n=int((xf-x_i)/h)                                   # Número de iteraciones que se ralizarán para resolver la ecuación
+    
+    # Se crea ciclo for para resolver la ecuación de acuerdo al método elegido
     for i in range(1,n+1):
-        xi = x[i-1]
+        xi = x[i-1]                                     
         yi = y[i-1]
-        k1 = f(xi,yi)
-        k2 = f(xi+h/2,yi+1/2*h*k1)
-        k3 = f(xi+h/2,yi+1/2*h*k2)
+
+        # Cálculo de las pendientes
+
+        k1 = f(xi,yi)                                   
+        k2 = f(xi+h/2,yi+h/2*k1)
+        k3 = f(xi+h/2,yi+h/2*k2)
         k4 = f(xi+h, yi+h*k3)
-        x.append(xi+h)
-        y.append(yi+1/6*h*(k1+2*k2+2*k3+k4))
+        x.append(xi+h)                                  # Se agregan términos a la lista x
+        y.append(yi+h/6*(k1+2*k2+2*k3+k4))              # Se agregan términos a la lista y
     return x,y
 
 fun_solve = lambda x,y:2*x*y
