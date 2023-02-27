02a89275e78810bea31bc3cfe90a1d82455d5503
nevocop <nelsone.barrerat@autonoma.edu.co>
2023-02-26 19:44:12 -0500

    Finaliza y comenta método euler mejorado

--- Proyecto_final.py
+++ Proyecto_final.py
@@ -4,7 +4,7 @@
 # MÉTODO DE EULER
 # Definición de la función Euler
 
-"""def euler(f,x_i,y_i,intervalo,xf):                      # Definición de la función con sus respectivos parámetros
+def euler(f,x_i,y_i,intervalo,xf):                      # Definición de la función con sus respectivos parámetros
     x=[x_i]                                             # Se crea la lista donde se almacenarán los valores de x
     y=[y_i]                                             # Se crea la lista donde se almacenarán los valores de y    
     h=intervalo                                         # Se crea la variable donde se almacena el intervalo a usar
@@ -12,13 +12,15 @@
     
     # Se crea ciclo for para resolver la ecuación por el método de Euler
     for i in range(1,n+1):
-        xi = x[i-1]
-        yi = y[i-1]
+        xi = x[i-1]                                     # Inicialización de la lista para x
+        yi = y[i-1]                                     # Inicialización de la lista para y
+        
+        # Se aplica la fórmula para método numérico de Euler
         x_euler = xi+h
         y_euler = yi+h*f(xi,yi)
-        x.append(x_euler)
-        y.append(y_euler)
-    return x,y"""
+        x.append(x_euler)                               # Se agregan términos a la lista x
+        y.append(y_euler)                               # Se agregan términos a la lista y
+    return x,y
 
 # MÉTODO DE EULER MEJORADO
 # Definición de la función Euler Mejorado
@@ -30,16 +32,22 @@ def euler_mej(f,x_i,y_i,intervalo,xf):                  # Definición de la func
     n=int((xf-x_i)/h)                                   # Número de iteraciones que se ralizarán para resolver la ecuación
     
     # Se crea ciclo for para resolver la ecuación por el método de Euler Mejorado
-    for i in range(1,n+1):
-        xi = x[i-1]
-        yi = y[i-1]
-        yi_m = yi + h*f(xi,yi)
-        yi = yi + h/2*(f(xi+h,yi_m) + f(xi,yi))
-        x.append(xi+h)
-        x.append(yi)
-    return (x,y)
+    for i in range (1, n+1):        
+        xi = x[i-1]                                     # Inicialización de la lista para x
+        yi = y[i-1]                                     # Inicialización de la lista para y
+        
+        # Se aplica la fórmula para método numérico de Euler Mejorado
+        yim = yi + h*f(xi,yi)
+        yi = yi + h/2*(f(xi+h,yim)+f(xi,yi))
+        x.append(xi+h)                                  # Se agregan términos a la lista x
+        y.append(yi)                                    # Se agregan términos a la lista y
+    return x,y
 
-"""# METODO DE RUNGE KUTTA
+fun_solve = lambda x,y:2*x*y
+x,ye=euler_mej(fun_solve,1,1,0.1,1.5)
+print(x,ye)
+
+# METODO DE RUNGE KUTTA
 # Definición de la función runge_kut
 
 def runge_kut(f,x_i,y_i,intervalo,xf):                  # Definición de la función con sus respectivos parámetros
@@ -53,7 +61,7 @@ def runge_kut(f,x_i,y_i,intervalo,xf):                  # Definición de la func
         xi = x[i-1]                                     
         yi = y[i-1]
 
-        # Cálculo de las pendientes
+        # Cálculo de las pendientes necesarias para el Método Runge Kutta
 
         k1 = f(xi,yi)                                   
         k2 = f(xi+h/2,yi+h/2*k1)
@@ -61,8 +69,6 @@ def runge_kut(f,x_i,y_i,intervalo,xf):                  # Definición de la func
         k4 = f(xi+h, yi+h*k3)
         x.append(xi+h)                                  # Se agregan términos a la lista x
         y.append(yi+h/6*(k1+2*k2+2*k3+k4))              # Se agregan términos a la lista y
-    return x,y"""
+    return x,y
 
-fun_solve = lambda x,y:2*x*y
-x,ye=euler_mej(fun_solve,1,1,0.1,1.5)
-print(x,ye)
+
