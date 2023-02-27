d1ec7fad5d8a03c31f02fa738804aa173f469adc
nevocop <nelsone.barrerat@autonoma.edu.co>
2023-02-26 22:22:12 -0500

    Solicitud de datos, elab graficas y tabla

--- Proyecto_final.py
+++ Proyecto_final.py
@@ -1,6 +1,11 @@
 """RESOLUCIÓN DE ECUACIONES CON TRES MÉTODOS NUMÉRICOS
           RUNGE KUTTA, EULER Y EULER MEJORADO"""
 
+# Se importan las librerias necesarias para el Programa
+
+import matplotlib.pyplot as plt                         # librería para graficar los tres métodos
+import pandas as pd                                     # librería para el manejo de tablas
+from math import *                                      # librería para calculos matemáticos
 # MÉTODO DE EULER
 # Definición de la función Euler
 
@@ -43,10 +48,6 @@ def euler_mej(f,x_i,y_i,intervalo,xf):                  # Definición de la func
         y.append(yi)                                    # Se agregan términos a la lista y
     return x,y
 
-fun_solve = lambda x,y:2*x*y
-x,ye=euler_mej(fun_solve,1,1,0.1,1.5)
-print(x,ye)
-
 # METODO DE RUNGE KUTTA
 # Definición de la función runge_kut
 
@@ -71,4 +72,33 @@ def runge_kut(f,x_i,y_i,intervalo,xf):                  # Definición de la func
         y.append(yi+h/6*(k1+2*k2+2*k3+k4))              # Se agregan términos a la lista y
     return x,y
 
+# Solicitud de datos para analizar
 
+funcion = input("Ingrese la función: ")                 # Se crea la variable donde se almacenará la función a resolver
+x_i = float(input("Ingrese valor inicial de x: "))      # Se ingresa el valor inicial de x
+y_i = float(input("Ingrese el valor inicial de y: "))   # Se ingresa el valor inicial de y
+y_f = float(input("Ingrese el valor final de y: "))     # Se ingresa el valor final de y
+h = float(input("Ingrese el intervalo que se va a utilizar: "))     # Se ingresa el intervalo que se va a utilizar para el análisis
+print('\n')                                             # Salto de línea
+fun_solve = lambda x,y:eval(funcion)                    # Se ingresa en una variable la función ingresada por el usuario y se evalua
+
+x,ye = euler(fun_solve,x_i,y_i,h,y_f)                   # Se aplica el método de Euler
+x,yem = euler_mej(fun_solve,x_i,y_i,h,y_f)              # Se aplica el método de Euler mejorado
+x,yrk = runge_kut(fun_solve,x_i,y_i,h,y_f)              # Se aplica el método de Runge Kutta
+
+# Se grafican las curvas de cada uno de los métodos utilizados en el programa
+plt.plot(x,ye,".-",label="Euler")
+plt.plot(x,yem,".-",label="Euler Mejorado")
+plt.plot(x,yrk,".-",label="RGK4")
+
+# Nomenclatura de la gráfica
+plt.title("Curvas de solución de los métodos utilizados")   # Se coloca título de la gráfica
+plt.legend(loc=4)                                           # Posición del título
+plt.xlabel("Eje X")                                         # Se nombra el eje X
+plt.ylabel("Eje Y")                                         # Se nombra el eje Y
+plt.grid()                                                  # Se coloca cuadrícula en el fondo de la gráfica                                             
+
+# Se crea tabla de valores de las tres soluciones
+df=pd.DataFrame(zip(x,ye,yem,yrk),columns=["x","Euler","Euler_Mej","RGK4"])     # Se genera y almacena la tabla en una variable
+print(df)                                                   # Se muestra la tabla en pantalla
+plt.show()                                                  # Se muestra la gráfica con las tres curvas
